cee4f446790b12cbf388fc349c472b68592aa778
surajkadm <68494225+surajkadm@users.noreply.github.com>
2023-03-24 19:13:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,20 +10,21 @@ const lookup = {
   'Y': 'L','Z': 'M', '?': '?', ',': ','
 };
 
-function rot13(encryptstr){
-	var words = str.split(" ");
-	var decodedArr=[];
-	for (let index = 0; index < words.length ; index++) {
-		var word=words[index];
-		var decodedWord="";
-		for(var j=0;j<word.length;j++){
-			var singleWord =word.charAt(j);
-			var decodedsingle=lookup[singleWord]
-			decodedWord+=decodedsingle;
-		}
-		decodedArr.push(decodedWord)
-	}
-	return decodedArr.join(" ")
+function rot13(encodedStr){
+   const words = encodedStr.split(" ");  // String to Array
+   let decodedArr = []; // Your Result goes here
+  // Only change code below this line
+   for(let i=0; i< words.length; i++){
+        const word = words[i]
+        let decoded_word  = ''
+        for(let j = 0; j < word.length; j++) {
+            const char = word.charAt(j)
+            const decoded_char = lookup[char]
+            decoded_word += decoded_char
+        }
+        decodedArr.push(decoded_word)
+     }
+        return decodedArr.join(" ");
 }
 
 module.exports = rot13;
