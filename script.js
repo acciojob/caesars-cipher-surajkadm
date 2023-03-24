e9b5646641ede0b977a5bf9b7c25bc1d3208d642
surajkadm <68494225+surajkadm@users.noreply.github.com>
2023-03-24 18:51:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Your Script here.
+function rot13(encryptstr){
+	var words = str.split(" ");
+	var decodedArr=[];
+	for (let index = 0; index < words.length ; index++) {
+		var word=words[index];
+		var decodedWord="";
+		for(var j=0;j<word.length;j++){
+			var singleWord=word.charAt(j)
+			var decodedsingle=(singleWord.charCodeAt(0)-13);
+			decodedWord+=decodedsingle;
+		}
+		decodedArr.push(decodedWord)
+	}
+	return decodedArr.join(" ")
+}
