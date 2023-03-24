e1040e845283b24151b122e0d4b427772814bd5e
surajkadm <68494225+surajkadm@users.noreply.github.com>
2023-03-24 19:10:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 // Your Script here.
+
+const lookup = {
+  'A': 'N','B': 'O','C': 'P','D': 'Q',
+  'E': 'R','F': 'S','G': 'T','H': 'U',
+  'I': 'V','J': 'W','K': 'X','L': 'Y',
+  'M': 'Z','N': 'A','O': 'B','P': 'C',
+  'Q': 'D','R': 'E','S': 'F','T': 'G',
+  'U': 'H','V': 'I','W': 'J','X': 'K',
+  'Y': 'L','Z': 'M', '?': '?', ',': ','
+};
+
 function rot13(encryptstr){
 	var words = str.split(" ");
 	var decodedArr=[];
@@ -6,8 +17,8 @@ function rot13(encryptstr){
 		var word=words[index];
 		var decodedWord="";
 		for(var j=0;j<word.length;j++){
-			var singleWord=word.charAt(j)
-			var decodedsingle=(singleWord.charCodeAt(0)-13);
+			var singleWord =word.charAt(j);
+			var decodedsingle=lookup[singleWord]
 			decodedWord+=decodedsingle;
 		}
 		decodedArr.push(decodedWord)
